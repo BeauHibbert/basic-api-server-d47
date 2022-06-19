a670168f5c0fa1a88a8bf121c76ad10ff14f423e
Beau Hibbert <beauhibbert789@gmail.com>
2022-06-18 18:27:43 -0700

    SSL config added

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'postgresql://localhost:5432/database_development';
 
-  const sequelize = new Sequelize(DATABASE_URL)
+  const sequelize = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }); 
 
 const TeamModel = teamSchema(sequelize, DataTypes);
 const FoodModel = foodSchema(sequelize, DataTypes);
